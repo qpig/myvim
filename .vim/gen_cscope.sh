a76ce1d348abb6fdfa5307ba5191299990eea00b
xiaoqia <564388878@qq.com>
2013-11-18 21:56:00 +0800

    a

--- .vim/gen_cscope.sh
+++ .vim/gen_cscope.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 #path=`pwd`
-#path=/root/FAS
-#path=/home/pig/zookeeper-3.4.5/src/c
-path=/home/pig/zookeeper-3.4.5/src
-#path=$HOME/iozone
-#path=$HOME/netcat-0.7.1
 data_path=$HOME/.vim
 
 echo generate cscope.out
