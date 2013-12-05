144afba667f58cbefb1502a6fa95dd32ae11f422
xiaoqia <xiaoqiang.zhuzhu@gmail.com>
2013-12-05 15:21:32 +0800

    a

--- .vim/gen_cscope.sh
+++ .vim/gen_cscope.sh
@@ -4,8 +4,8 @@ path=/home/pig/memcached-1.4.15
 data_path=$HOME/.vim
 
 echo generate cscope.out
-find $path -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" -o -name "*.java" > $HOME/.vim/cscope.files
-sudo cscope -bk -i $HOME/.vim/cscope.files
+find $path -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" -o -name "*.java" > $data_path/cscope.files
+sudo cscope -bk -i $data_path/cscope.files
 mv -f cscope.* $data_path
 
 #ctags --c-kinds=+p --fields=+S -R /usr/include/
@@ -14,5 +14,5 @@ sudo ctags -R --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q $path
 mv -f tags $data_path
 
 echo generate TAGS
-sudo etags -i $HOME/.vim/cscope.files
+sudo cat $data_path/cscope.files | xargs etags -
 mv -f TAGS $data_path
