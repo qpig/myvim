63c3f48ad4c03b0fe0c28b5e00711a2f1cf61177
xiaoqia <xiaoqiang.zhuzhu@gmail.com>
2014-04-03 12:54:23 +0800

    gen_cscope can use pasths

--- .vim/gen_cscope.sh
+++ .vim/gen_cscope.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
-#path=`pwd`
-path=/home/pig/memcached-1.4.15
+PATHS=(/home/pig/memcached-1.4.15)
 data_path=$HOME/.vim
 
 echo generate cscope.out
-find $path -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" -o -name "*.java" > $data_path/cscope.files
+rm $data_path/cscope.files -f
+for index in ${!PATHS[*]}
+do
+	path=${PATHS[index]}
+	echo find $path
+	find $path -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.hpp" -o -name "*.java" >> $data_path/cscope.files
+done
 sudo cscope -bk -i $data_path/cscope.files
 mv -f cscope.* $data_path
 
-#ctags --c-kinds=+p --fields=+S -R /usr/include/
 echo generate tags
-sudo ctags -R --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q $path
+sudo ctags -R --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q -L $data_path/cscope.files
 mv -f tags $data_path
 
 echo generate TAGS
-sudo cat $data_path/cscope.files | xargs etags -
+sudo cat $data_path/cscope.files | xargs etags --language=c++ --language=c -
 mv -f TAGS $data_path
+exit 0
+
